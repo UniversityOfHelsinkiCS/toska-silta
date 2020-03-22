896b98b3c99bb23a60e62539e91ba216640cdb54
Jami Kousa <jami.kousa@helsinki.fi>
2020-03-22 19:27:32 +0200

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const router = require('koa-router')()
+const axios = require('axios')
+const app = new Koa()
+const PORT = process.env.PORT || 3000
+const DISCORD_HOOK = process.env.DISCORD_HOOK || ''
+
+app.use(bodyParser())
+
+
+router.post('/slack/event', async ctx => {
+  if (ctx.request.body.challenge) {
+    ctx.body = ctx.request.body.challenge
+    return
+  }
+  const eventBody = ctx.request.body
+  const { text, channel, user } = eventBody.event
+
+  const content = `On ${channel}: ${text}`
+  const webhookPayload = {
+    username: user,
+    content,
+  }
+
+  await axios.post(DISCORD_HOOK, webhookPayload)
+
+  ctx.status = 200
+})
+
+app.use(router.routes());
+
+app.listen(PORT)
