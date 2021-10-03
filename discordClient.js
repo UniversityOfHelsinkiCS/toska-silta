05d306ff16db3b5c31a218be7b281db71a324947
Gert Adamson <adamson.gert@hotmail.com>
2021-10-03 22:42:57 +0300

    Add more intents

--- discordClient.js
+++ discordClient.js
@@ -1,7 +1,16 @@
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN || ''
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [] });
+const intents = [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+];
+
+const client = new Client({ intents });
 client.login(DISCORD_BOT_TOKEN)
 
 module.exports = client
