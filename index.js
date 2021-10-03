e6b5323e55fbb5234ca789f082c4e92ffab3f82a
Gert Adamson <adamson.gert@hotmail.com>
2021-10-03 21:50:22 +0300

    Fix the fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 const PORT = process.env.PORT || 3000
 
-require('./discrodClient')
+require('./discordClient')
 
 require('./routerDiscordToSlack')
 
