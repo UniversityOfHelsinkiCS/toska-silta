9b3252c6b86655830f4372300fb539e2e4a108c7
Gert Adamson <adamson.gert@hotmail.com>
2021-10-04 18:57:22 +0300

    Test webhook.send with content & file

--- routerSlackToDiscord.js
+++ routerSlackToDiscord.js
@@ -13,12 +13,19 @@ const sentFileCache = new NodeCache({ stdTTL: 130 })
 const sendFileToDiscord = async (url, userInfo, webhook) => {
   if (sentFileCache.has(url)) return
   sentFileCache.set(url, true)
-  const { data } = await axios.get(url, { responseType: 'stream', headers: { 'Authorization': `Bearer ${SLACK_BOT_TOKEN}` } })
-  const form = new FormData()
-  form.append('username', userInfo.username)
-  form.append('avatar_url', userInfo.avatar_url)
-  form.append('file', data)
-  form.submit(webhook.url)
+  // const { data } = await axios.get(url, { responseType: 'stream', headers: { 'Authorization': `Bearer ${SLACK_BOT_TOKEN}` } })
+  // const form = new FormData()
+  // form.append('username', userInfo.username)
+  // form.append('avatar_url', userInfo.avatar_url)
+  // form.append('file', data)
+  // form.submit(webhook.url)
+
+  await webhook.send({
+    content: "test",
+    username: userInfo.username,
+    avatarURL: userInfo.avatar_url,
+    files: [url]
+  })
 }
 
 const sendMessageToDiscord = async (content, username, avatarURL, webhook) => {
