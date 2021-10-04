ab6a1fe12b347fb2aa9a10c8939958f0163183c7
Gert Adamson <adamson.gert@hotmail.com>
2021-10-04 18:31:41 +0300

    try new way of file sending

--- routerSlackToDiscord.js
+++ routerSlackToDiscord.js
@@ -13,12 +13,12 @@ const sentFileCache = new NodeCache({ stdTTL: 130 })
 const sendFileToDiscord = async (url, userInfo, webhook) => {
   if (sentFileCache.has(url)) return
   sentFileCache.set(url, true)
-  const { data } = await axios.get(url, { responseType: 'stream', headers: { 'Authorization': `Bearer ${SLACK_BOT_TOKEN}` } })
-  const form = new FormData()
-  form.append('username', userInfo.username)
-  form.append('avatar_url', userInfo.avatar_url)
-  form.append('file', data)
-  form.submit(webhook.url)
+
+  await webhook.send({
+    username: userInfo.username,
+    avatarURL: userInfo.avatar_url,
+    files: [url]
+  })
 }
 
 const sendMessageToDiscord = async (content, username, avatarURL, webhook) => {
@@ -50,7 +50,7 @@ const handleSlackFile = async (ctx, event, webhook) => {
 
   const { data: fileData } = await axios.get(fileInfoUrl)
   const userInfo = await getInfoForSlackUser(user_id)
-  const actualFileUrl = fileData.file.url_private_download
+  const actualFileUrl = fileData.file.url_private
   await sendFileToDiscord(actualFileUrl, userInfo, webhook)
 
   ctx.status = 200
@@ -60,7 +60,7 @@ const handleFileShare = async (ctx, event, webhook) => {
   const userInfo = await getInfoForSlackUser(event.user)
   await Promise.all(event.files.map(file => {
     console.log('file', file)
-    const url = file.url_private_download
+    const url = file.url_private
     return sendFileToDiscord(url, userInfo, webhook)
   }))
   ctx.status = 200
